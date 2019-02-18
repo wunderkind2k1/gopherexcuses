9c83f5157705e3ebcca345db51eb760293d86046
Sven Hohlfeld <sho1975@gmail.com>
2019-02-18 07:53:55 +0100

    more tests

--- gopherexcuses_test.go
+++ gopherexcuses_test.go
@@ -56,3 +56,38 @@ func TestHandleErrorIfExists(t *testing.T) {
 		t.Fatalf("did not recieve the expected message: %s", outString)
 	}
 }
+
+func TestSimpleExtraction(t *testing.T) {
+	testLoader := &LoaderStruct{}
+	link, err := testLoader.extract("<a>http://www.thisisthelink.com</a>")
+	if err != nil {
+		t.Fatalf("Link not extracted. Error: %s", err)
+	}
+	if link != "http://www.thisisthelink.com" {
+		t.Fatalf("Extracted wrong Link: %s", link)
+	}
+}
+
+func TestAdvancedExtraction(t *testing.T) {
+	testLoader := &LoaderStruct{}
+	link, err := testLoader.extract(`<body>div style='font-size:3em'>
+	<script><!-- alert('this is js');--></script>
+	<a href='http://www.foo.com'>http://www.thisisthelink2.com</a></div><body>`)
+	if err != nil {
+		t.Fatalf("Link not extracted. Error: %s", err)
+	}
+	if link != "http://www.thisisthelink2.com" {
+		t.Fatalf("Extracted wrong Link: %s", link)
+	}
+}
+
+func TestNothingfoundExtraction(t *testing.T) {
+	testLoader := &LoaderStruct{}
+	link, err := testLoader.extract("<div>no link here</div>")
+	if link != "" {
+		t.Fatalf("No link expected. Found link: %s", link)
+	}
+	if err == nil {
+		t.Fatalf("Expected error not returned")
+	}
+}
